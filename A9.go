484b9e42802df045fd128c060e0c1df139faf7e6
Ethan <ethanothepro@gmail.com>
2023-03-16 22:03:47 -0700

    test num,id,string

--- A9.go
+++ A9.go
@@ -1,51 +1,166 @@
 package main
 
-
-import "fmt"
-
+import (
+	"errors"
+	"fmt"
+)
 
 type ExprC interface{}
 
 type NumC struct {
-    Num int
-}
-
-type IfC struct {
-    Test ExprC
-    Then ExprC
-    Else ExprC
+	N int
 }
 
 type IdC struct {
-    S string
-}
-
-type AppC struct {
-    Fun ExprC
-    Arg []ExprC
-}
-
-type LamC struct {
-    Args []string
-    Body ExprC
+	S string
 }
 
 type StrC struct {
+	S string
+}
+
+type AppC struct {
+	Fun  ExprC
+	Args []ExprC
+}
+
+type LamC struct {
+	Args []string
+	Body ExprC
+}
+
+type IfC struct {
+	If   ExprC
+	Then ExprC
+	Else ExprC
+}
+
+type Value interface{}
+
+type NumV struct{
+    N int
+}
+
+type BoolV struct{
+    Bool bool
+}
+
+type StrV struct{
     S string
 }
 
+type Closer interface{}
 
+type ClosV struct {
+	Args []string
+	Body ExprC
+	Env  []Binding
+}
+
+type Binding struct {
+	Name string
+	Val  Value
+}
+
+func getBinop(op string, l int, r int) (interface{}, error) {
+	switch op {
+	case "equal?":
+		return l == r, nil
+	case "+":
+		return l + r, nil
+	case "-":
+		return l - r, nil
+	case "*":
+		return l * r, nil
+	case "/":
+		if r == 0 {
+			return nil, errors.New("division by zero")
+		}
+		return l / r, nil
+	case "<=":
+		return l <= r, nil
+	default:
+		return nil, errors.New("invalid binop syntax")
+	}
+}
+
+func getEnv(s []string, l []Value, env []Binding) []Binding {
+	if len(s) == 0 {
+		return env
+	}
+	return append([]Binding{Binding{s[0], l[0]}}, getEnv(s[1:], l[1:], env)...)
+}
+
+func lookup(forVal string, env []Binding) Value {
+	if len(env) == 0 {
+		panic("user-error No value match!")
+	} else {
+		if forVal == env[0].Name {
+			return env[0].Val
+		} else {
+			return lookup(forVal, env[1:])
+		}
+	}
+}
+
+func interp(e ExprC, env []Binding) Value {
+	switch e := e.(type) {
+	case NumC:
+		return NumV{N : e.N}
+	case StrC:
+		return StrV{S : e.S}
+	case IdC:
+		return lookup(e.S, env)
+	case LamC:
+		return ClosV{e.Args, e.Body, env}
+	case IfC:
+		ifVal := interp(e.If, env)
+		if (ifVal == BoolV{Bool : true}) {
+			return interp(e.Then, env)
+		} else if (ifVal == BoolV{Bool : false}) {
+			return interp(e.Else, env)
+		} else {
+			// Would be helpful to print out invalid value
+			panic("if condition should be boolean value")
+		}
+	case AppC:
+		fd := interp(e.Fun, env)
+		fArgs := make([]Value, len(e.Args))
+		for i, arg := range e.Args {
+			fArgs[i] = interp(arg, env)
+		}
+		switch fd := fd.(type) {
+		case func(Value, Value) Value:
+			if len(fArgs) != 2 {
+				panic("invalid number of arguments")
+			}
+			return fd(fArgs[0], fArgs[1])
+		case ClosV:
+			fEnv := getEnv(fd.Args, fArgs, fd.Env)
+			return interp(fd.Body, fEnv)
+		default:
+			panic("invalid application")
+		}
+	default:
+		panic("invalid expression")
+	}
+}
 
 
 
 func main() {
-    fmt.Println("Hello, World!")
-    str := StrC{S: "hello world"}
-    testExprC := AppC{Fun: IdC{S: "+"}, Arg: []ExprC{NumC{Num: 3}, NumC{Num: 4}}}
-    fmt.Println(str)
-    fmt.Println(testExprC)
-}
-
-func Add(a, b int) int {
-    return a + b
+	fmt.Println("hello world")
+    //testExprC := AppC{Fun: IdC{S: "+"}, Args: []ExprC{NumC{N: 3}, NumC{N: 4}}}
+    /*
+    topEnv := []Binding{
+       
+        Binding{Name: "+", Val: createPrimFunc("+")},
+        Binding{Name: "/", Val: createPrimFunc("/")},
+        Binding{Name: "*", Val: createPrimFunc("*")},
+        Binding{Name: "-", Val: createPrimFunc("-")},
+  
+    }
+    */
+    fmt.Println(interp(NumC{N : 2}, []Binding{}))
+    fmt.Println(NumV{N : 2})
 }
--- A9_test.go
+++ A9_test.go
@@ -4,9 +4,41 @@ import (
 	"testing"
 )
 
+/*
 func TestAdd(t *testing.T) {
 	result := Add(2, 3)
 	if result != 5 {
 		t.Errorf("Expected Add(2, 3) to be 5, but got %d", result)
 	}
 }
+*/
+
+func TestInterpNum(t *testing.T) {
+	
+	result := interp(NumC{N : 2}, []Binding{})
+	if (result != NumV{N : 2}){
+		t.Errorf("Expected NumV(2)")
+	}
+
+}
+
+func TestInterpId(t *testing.T) {
+	
+	result := interp(IdC{S : "x"}, []Binding{Binding{Name: "y", Val: NumV{N : 4}},
+											 Binding{Name: "x", Val: NumV{N : 2}}})
+	if (result != NumV{N : 2}){
+		t.Errorf("Expected NumV(2)")
+	}
+
+}
+
+func TestInterpString(t *testing.T) {
+	
+	result := interp(StrC{S : "hello"}, []Binding{})
+	if (result != StrV{S: "hello"}){
+		t.Errorf("Expected NumV(2)")
+	}
+
+}
+
+
